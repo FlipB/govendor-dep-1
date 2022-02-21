9d83f9059f7fc2e2e4e3e9bf4871539fda72c7b9
Filip Björck <filip.bjorck@gmail.com>
2022-02-21 15:56:16 +0100

    Clarify output message

--- dep.go
+++ dep.go
@@ -3,10 +3,10 @@ package sayhello
 import "fmt"
 
 // depVersion matches the git tag of the module for this dependency
-const depVersion = "v2.0.0"
+const depVersion = "v2.0.1"
 
 func say(greeting, name string) {
-	fmt.Printf("%s %s (from %s)", greeting, name, depVersion)
+	fmt.Printf("%s %s (from dep %s)", greeting, name, depVersion)
 }
 
 func Hello(name string) {
